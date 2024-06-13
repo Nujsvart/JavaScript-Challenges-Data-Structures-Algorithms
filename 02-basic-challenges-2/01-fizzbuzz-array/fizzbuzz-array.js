580e88fb653789f346cece5816af68a0fea6a44d
Esra C <kampfar@gmail.com>
2024-06-13 17:07:21 +0300

    basic challenges-2

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,15 @@
-function fizzBuzzArray() {}
+const fizzBuzzArray = n => {
+  const numbers = Array.from({ length: n }, (_, i) => i + 1);
+
+  return numbers.map(n =>
+    n % 3 === 0 && n % 5 === 0
+      ? "FizzBuzz"
+      : n % 3 === 0
+      ? "Fizz"
+      : n % 5 === 0
+      ? "Buzz"
+      : n
+  );
+};
 
 module.exports = fizzBuzzArray;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,7 @@
-function arrayIntersection() {}
+/* const arrayIntersection = (arr1, arr2) =>
+  arr1.reduce((acc, curr) => (arr2.includes(curr) ? [...acc, curr] : acc), []);
+ */
+
+const arrayIntersection = (arr1, arr2) => arr1.filter(el => arr2.includes(el));
 
 module.exports = arrayIntersection;
